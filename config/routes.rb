f5686a1cf42721ede3ad2330b954ae95678e9125
taro.tanaka <kwnhe231_0411@yahoo.co.jp>
2020-02-21 02:44:49 +0000

    サインイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
+  
+  #ユーザーの新規登録urlを/signupにするため、なくてもよい
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
 end

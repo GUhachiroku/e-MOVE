103f1218cb03338d1f2daf86a0f61624bcdf8e14
taro.tanaka <kwnhe231_0411@yahoo.co.jp>
2020-03-20 07:32:18 +0000

    スプリットレイアウト実装、一覧表示の細かな修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_074307) do
+ActiveRecord::Schema.define(version: 2020_03_18_062048) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_03_17_074307) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
